5e2ee7e7809fb0d3cc4aaef1b5a7b6eeda22209e
huynhnhathuy0812 <huynhnhathuy0812@gmail.com>
2025-11-01 21:38:58 +0700

    bai tap 2.3

---
+++ src/baitap23/Author.java
@@ -0,0 +1,27 @@
+package baitap23;
+
+public class Author {
+    public String name;
+    public String email;
+
+    public Author(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String toString() {
+        return "Author[name=" + name + ",email=" + email + "]";
+    }
+}
