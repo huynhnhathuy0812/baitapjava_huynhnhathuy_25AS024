6f463b39273c76f74b5ae6a0463b60b18c960b1c
huynhnhathuy0812 <huynhnhathuy0812@gmail.com>
2025-12-25 22:54:08 +0700

    bai tap 6.8

--- src/baitap67/Animal.java
+++
@@ -1,6 +0,0 @@
-package baitap67;
-
-abstract class Animal {
-    abstract public void greeting();
-}
-
--- src/baitap67/Cat.java
+++
@@ -1,8 +0,0 @@
-package baitap67;
-
-class Cat extends Animal {
-    @Override
-    public void greeting() {
-        System.out.println("Meow!");
-    }
-}
